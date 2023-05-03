8bec1f5babdd391ed020a926bde5ff42b9639ff2
Brennan Phifer <98411964+NovaGamer368@users.noreply.github.com>
2023-05-03 23:14:19 +0000

    Got the api call to text

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-print("TESTING GITHUB")
-print("Brennan Chill")
-print("Angelina")
-print("Kill me")
-print("Is VSCODE stupid? Answer may shock you")
+import os
+import json
+import requests
+import openai
+
+
+apiKey = "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"
+
+openai.api_key = apiKey
+
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt= "Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
+
+# Load the JSON data as a dictionary
+data = json.loads(json.dumps(response))
+
+# Access the 'choices' key
+choices = data['choices']
+
+# Print the choices
+for choice in choices:
+    print(choice['text'])
