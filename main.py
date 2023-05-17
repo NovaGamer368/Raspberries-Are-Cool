23afcd71c69049968eef9e4f3ce6f01d69330874
Brennan Phifer <98411964+NovaGamer368@users.noreply.github.com>
2023-05-17 22:05:35 +0000

    pip installs and function main

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-import os
+#py -m install -r requirements.txt
 import json
-import requests
 import openai
 import config
 
-
-
- # INSERT API KEY FOR CHATGPT
-
 openai.api_key = config.api_key
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt= "list me ingrediants for shrimp scampi",
-  temperature=0,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.2,
-  presence_penalty=0
-)
+def api_call(string_recording):
+  #API call
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt= string_recording,
+    temperature=0,
+    max_tokens=100,
+    top_p=1,
+    frequency_penalty=0.2,
+    presence_penalty=0
+  )
 
-# Load the JSON data as a dictionary
-data = json.loads(json.dumps(response))
+  # Load the JSON data as a dictionary
+  data = json.loads(json.dumps(response))
 
-# Access the 'choices' key
-choices = data['choices']
+  # Access the 'choices' key
+  choices = data['choices']
 
-# Print the choices
-for choice in choices:
-  print(choice['text'])
+  # Print the choices
+  for choice in choices:
+    print(choice['text'])
